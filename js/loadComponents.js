d7b0a433726acc2e2a2e2bfa8da3449fd76366a1
samimosmansabuj <samim.o.sabuj01@gmail.com>
2026-09-16 01:24:56 +0600

    update all assets link!!!

--- js/loadComponents.js
+++ js/loadComponents.js
@@ -67,21 +67,21 @@ async function initComponents() {
         // Load Meta Manager if not already loaded
         if (typeof window.updateMetaTags !== "function") {
             const sm = document.createElement("script");
-            sm.src = "js/meta-manager.js?v=27";
+            sm.src = "js/meta-manager.js?v=28";
             document.head.appendChild(sm);
         }
 
         // Load PWA Installer if not already loaded
         if (typeof window.showPwaInstallPrompt !== "function") {
             const sp = document.createElement("script");
-            sp.src = "js/pwa-installer.js?v=27";
+            sp.src = "js/pwa-installer.js?v=28";
             document.body.appendChild(sp);
         }
 
         // Ensure cart.js is loaded on any page
         if (typeof loadCartItems !== "function") {
             const s = document.createElement("script");
-            s.src = "js/cart.js?v=27";
+            s.src = "js/cart.js?v=28";
             s.onload = () => {
                 if (typeof updateCartCountFromBackend === "function") updateCartCountFromBackend();
                 if (typeof loadCartItems === "function") loadCartItems();
--- sw.js
+++ sw.js
@@ -10,18 +10,18 @@ const PRECACHE_ASSETS = [
     '/',
     '/offline.html',
     '/manifest.json',
-    '/css/home.css?v=27',
-    '/css/products.css?v=27',
-    '/css/product.css?v=27',
+    '/css/home.css?v=28',
+    '/css/products.css?v=28',
+    '/css/product.css?v=28',
     '/images/safa-world-logo.jpg',
     '/images/logo-loader.png',
     '/images/icons/icon-192x192.png',
     '/images/icons/icon-512x512.png',
-    '/js/spa-router.js?v=272113',
-    '/js/loadComponents.js?v=27',
-    '/js/main.js?v=272112',
-    '/js/meta-manager.js?v=27',
-    '/js/pwa-installer.js?v=27'
+    '/js/spa-router.js?v=282113',
+    '/js/loadComponents.js?v=28',
+    '/js/main.js?v=282112',
+    '/js/meta-manager.js?v=28',
+    '/js/pwa-installer.js?v=28'
 ];
 
 const STATIC_ROUTES = {
